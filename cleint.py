1f33743acaf78574dee4819b6cfa6b6c4d3d822b
Tomasmartaf <tomas.martinek07@seznam.cz>
2026-04-25 21:15:36 +0200

    Bloky jako tlačítka

--- cleint.py
+++ cleint.py
@@ -11,7 +11,6 @@ velikostSurface1_x = 50
 velikostSurface1_y = 50
 
 # socket connection setup #
-# input for ip and connection to the second player/server #
 HOST = ''
 PORT = 0
 while HOST == '' or PORT == 0:
@@ -24,32 +23,28 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # pygame setup #
 pg.init()
 screen = pg.display.set_mode((sirkaHry, vyskaHry))
-screen.fill('grey50')
 pg.display.set_caption("Spoj 4")
 clock = pg.time.Clock()
 
 # Proměnné pro game ui # 
-pocetCtverecku = 0
+pocetCtverecku = 10
 size = 50
-space = 0
 spacing = size + 50
 surfaceBlockSize_x = size
 surfaceBlockSize_y = size
 surface = []
-
+blockSouradnice_x = []
+blockSouradnice_y = []
+    
 for i in range(10):
     surface.append(pg.Surface((surfaceBlockSize_x, surfaceBlockSize_y)))
 
-# Surface na testing #
-bomba_surface = pg.Surface((velikostSurface1_x, velikostSurface1_y))
-bomba_surface.fill('azure')
-
-
 def mouseClick():
     mouse_pozice = pg.mouse.get_pos()
-    if mouse_pozice[0] > ctverecek_x and mouse_pozice[1] > ctverecek_y:
-        if mouse_pozice[0] < (ctverecek_x + velikostSurface1_x) and mouse_pozice[1] < (ctverecek_y + velikostSurface1_y):
-            print('tlacitko i guess')
+    for i in range (pocetCtverecku):
+        if mouse_pozice[0] > blockSouradnice_x[i] and mouse_pozice[1] > blockSouradnice_y[i]:
+            if mouse_pozice[0] < (blockSouradnice_x[i] + velikostSurface1_x) and mouse_pozice[1] < (blockSouradnice_y[i] + velikostSurface1_y):
+                print('tlacitko i guess')
 
 while True:    
     for event in pg.event.get():
@@ -59,14 +54,17 @@ while True:
         if event.type == pg.MOUSEBUTTONDOWN:
             mouseClick()
 
-    ctverecek_x = int(prostredHry_x)
-    ctverecek_y = int(prostredHry_y-velikostSurface1_y)
-    screen.blit(bomba_surface,(ctverecek_x, ctverecek_y))
-
-    if pocetCtverecku < 3:
+    screen.fill('grey50')
+    
+    # kresleni 1. rady, test #
+    space = 0
+    for i in range(pocetCtverecku):
         space += spacing
-        screen.blit(surface.__getitem__(pocetCtverecku),(velikostSurface1_x + space, velikostSurface1_y))
-    pocetCtverecku += 1
+        block_x = velikostSurface1_x + space
+        block_y = velikostSurface1_y
+        blockSouradnice_x.append(block_x)
+        blockSouradnice_y.append(block_y)
+        screen.blit(surface[i],(block_x, block_y))
 
     pg.display.update()
     clock.tick(60) #framerate hodin
